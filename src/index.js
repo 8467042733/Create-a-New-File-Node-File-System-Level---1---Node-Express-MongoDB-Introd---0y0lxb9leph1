0043da15ffcbe40c9bcf8ccf5256b671d8ae3ae4
8467042733 <rupeshpal25112004@gmail.com>
2023-12-28 13:42:11 +0530

    add some code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  try {
+    await fs.writeFile(fileName, fileContent);
+    console.log(`File ${fileName} created and data written successfully!`);
+  } catch (err) {
+    console.error(`Error writing to file ${fileName}: ${err.message}`);
+  }
 };
-
+writeFile(fileName,fileContent);
 module.exports = { writeFile };
